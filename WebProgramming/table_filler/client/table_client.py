96bf0484e754e651dd63ea208ce34a70541f30af
Nikolay Pashov <nikolay.pashov@halodx.com>
2022-05-12 11:38:45 +0300

    Finish web programming project

--- WebProgramming/table_filler/client/table_client.py
+++ WebProgramming/table_filler/client/table_client.py
@@ -1,9 +1,32 @@
 import requests
 import json
+import random
+
+def request_generator(w = 100, h = 100, workers = 5, default = 5):
+    
+    req = {
+        "width" : w,
+        "height" : h,
+        "workers" : workers,
+        "default" : default,
+        "fill_data" : []}
+
+    for i in range(w):
+        for j in range(h):
+            value = random.randint(1, 5000000)
+            req["fill_data"].append([i, j, value])
+
+    with open('generated_request.json', 'w') as f:
+        json.dump(req, f)
+    return req
+
 if __name__ == '__main__':
     
     with open('request.json') as f:
         req = json.load(f)
     print(req)
 
-    r = requests.get('http://localhost:5000/', json=req)
+    r = requests.get('http://localhost:5000/', json=request_generator(w = 1000, h = 1000, workers=5))
+    print(r.json)
+    print(len(r.content))
+
--- WebProgramming/table_filler/server/table_filler.py
+++ WebProgramming/table_filler/server/table_filler.py
@@ -1,7 +1,10 @@
+from textwrap import fill
 from flask import Flask
 from matplotlib.pyplot import table
 from flask import request
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import time
+import numpy as np
 
 from table import Table
 
@@ -9,39 +12,82 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def table_controller():
-    print(request.method)
-    print(request.json)
-    fill_table(request.json)
-    return "200"
+  
+    print("Request received.")
+    return str(handle_fill_table_request(request.json))
 
 def fill_table_cell(cell_fill_data, table):
     
-    print("call")
     x = cell_fill_data[0]
     y = cell_fill_data[1]
     value = cell_fill_data[2]
     table[x][y] = value
 
-    return None
 
-def fill_table(fill_data):
+def handle_fill_table_request(fill_data):
 
     w = fill_data['width']
     h = fill_data['height']
     default = fill_data['default']
+    workers = fill_data['workers']
 
     table = list()
-    for i in range(h):
+    for i in range(w):
         row = list()
-        for j in range(w):
+        for j in range(h):
             row.append(default)
         table.append(row)
 
-    with ThreadPoolExecutor(max_workers=3) as executor:
-        res = executor.map(lambda d: fill_table_cell(d, table))
-        futures = [executor.submit(fill_table_cell, d, table) for d in fill_data['fill_data']]
 
-    #print(futures.result)
-    #print(list(map(lambda d: fill_table_cell(d, table), fill_data['fill_data'])))
-    print(table)
+    print("empty table created ")
+
+    start_concurrent = time.time()
+    table_concurrent = fill_table_concurrently(fill_data['fill_data'], table.copy(), workers)
+    end_concurrent = time.time()
+    time_concurrent = end_concurrent - start_concurrent
+
+    start_sync = time.time()
+    table_sync = fill_table_single_thread(fill_data['fill_data'], table)
+    end_sync = time.time()
+    time_sync = end_sync - start_sync
+
+    print("Time with concurrency: ", time_concurrent)
+    print("Time with single thread: ", time_sync)
+
+    return table
+
+    
         
+def fill_table_concurrently(fill_data, table, workers):
+
+    split_data = np.array_split(fill_data, workers)
+    print("start submit")
+    with ThreadPoolExecutor(max_workers=workers) as executor:
+        #futures = [executor.submit(fill_table_cell, d, table) for d in fill_data]
+        for i, data in enumerate(split_data):
+            executor.submit(fill_table_single_thread, data, table, i)
+
+    print("end submit")
+    return table
+
+def fill_table_multiprocess(fill_data, table, workers):
+
+
+    split_data = np.array_split(fill_data, workers)
+    print("start submit")
+    with ProcessPoolExecutor(max_workers=workers) as executor:
+        #futures = [executor.submit(fill_table_cell, d, table) for d in fill_data]
+        for i, data in enumerate(split_data):
+            executor.submit(fill_table_single_thread, data, table, i)
+
+    print("end submit")
+    return table
+        
+    
+    return table
+
+def fill_table_single_thread(fill_data, table, id=1):
+    
+    print("fill data len", len(fill_data))
+    for fd in fill_data: fill_table_cell(fd, table); #print("pooo", id)
+    return table
